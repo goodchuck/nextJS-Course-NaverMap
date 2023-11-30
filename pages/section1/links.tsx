5572f70e5f9f46f1ee873e5d523ce97309c96576
goodchuck <y502100@naver.com>
2023-11-30 16:02:03 +0900

    1-3 next/link로 routing

---
+++ pages/section1/links.tsx
@@ -0,0 +1,16 @@
+import Link from 'next/link';
+
+export default function Links() {
+    return (
+        <main>
+            <h1>Links</h1>
+            {/*<div style={{ height: '200vh' }} />*/}
+
+            {/* eslint-disable-next-line @next/next/no-html-link-for-pages */}
+            {/*<a href="/section1/getStaticProps">/getStaticProps</a>*/}
+
+            <Link href="/section1/getStaticProps">/getStaticProps</Link>
+            {/** https://github.com/vercel/next.js/blob/canary/packages/next/client/link.tsx#L487 */}
+        </main>
+    );
+}
