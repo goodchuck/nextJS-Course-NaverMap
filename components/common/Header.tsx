055b155f59414835a1e6a050bff054862257b05e
goodchuck <y502100@naver.com>
2023-12-02 15:01:08 +0900

    2-3 Header 컴포넌트 완성하기

--- components/common/Header.tsx
+++ components/common/Header.tsx
@@ -1,17 +1,26 @@
 import React from 'react';
 import Link from 'next/link';
+import Image from 'next/image';
 import styles from '../../styles/header.module.scss';
 
-interface Props {}
+interface Props {
+  rightElements?: React.ReactElement[];
+}
 
-const HeaderComponent = ({}: Props) => {
+const HeaderComponent = ({ rightElements }: Props) => {
   return (
     <header className={styles.header}>
       <div className={styles.flexItem}>
         <Link href="/" className={styles.box}>
-          <img src="/inflearn.png" width={110} height={20} alt="인프런 로고" />
+          <Image
+            src="/inflearn.png"
+            width={110}
+            height={20}
+            alt="인프런 로고"
+          />
         </Link>
       </div>
+      {rightElements && <div className={styles.flexItem}>{rightElements}</div>}
     </header>
   );
 };
